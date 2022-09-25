04d0c0411dddcd56e4e5891198ecab4853d88b17
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2022-09-25 11:43:10 -0300

    UPDATE

---
+++ springcloudstorage/springcloudstorage/src/test/java/com/patrickriibeiro/springcloudstorage/SpringcloudstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickriibeiro.springcloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
